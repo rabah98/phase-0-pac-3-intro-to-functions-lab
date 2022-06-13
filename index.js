47efe2a4d484ef7d1a21c79ef079af42dee82317
Rabah <rmerzoug471@gmail.com>
2022-06-13 11:06:16 -0500

    commit tree 26792477c39dd9b57855d6d6c1538be070adbddc

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(string) {
+    return string.toLowerCase();
+}
+function logShout(string) {
+    console.log(string.toUpperCase());
+}
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+} 
+function sayHiToHeadphonedRoommate(string) { 
+    
+    const Up = string.toUpperCase();
+    const Lo = string.toLowerCase
+    
+    switch (string) {
+        case Lo:
+            "I can't hear you!"
+        case Up:
+            "YES INDEED!"
+        case "Let's have dinner together!":
+            "I would love to!" 
+
+    }
+
+    }
